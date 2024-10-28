6bee2b37bb4f757f0eaa5eb381a1c831e0ca58b6
EstebanCastilloTello <esteban.castillo@sansano.usm.cl>
2024-10-28 15:14:49 -0300

    Se agrego direccionamiento de registro a resumen

--- src/pages/RegistrerPage.jsx
+++ src/pages/RegistrerPage.jsx
@@ -3,27 +3,13 @@ import { Link, useNavigate } from 'react-router-dom';
 
 const RegistrerPage = () => {
   const navigate = useNavigate();
-<<<<<<< HEAD
-
-  const handleSubmit = (e) => {
-    e.preventDefault(); // Evita el comportamiento por defecto del formulario
-
-    // Redirige a la página después del registro
-    navigate('/login');
-  };
-=======
   const goToLogin = () => navigate('/resumen');
->>>>>>> e2a91ee4734231844e3f5c1aa15dba2ba983402b
 
   return (
     <div className="registrer-page">
       <div className="card">
         <h2 className="card__title">Registro</h2>
-<<<<<<< HEAD
-        <form onSubmit={handleSubmit}>
-=======
         <form onSubmit={goToLogin}>
->>>>>>> e2a91ee4734231844e3f5c1aa15dba2ba983402b
           <div className="form-group">
             <label htmlFor="nombre">Nombre Completo</label>
             <input type="text" id="nombre" name="nombre" required />
