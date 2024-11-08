dc8d21b1a150f8d8dca11cb1243880b4361906ee
EstebanCastilloTello <esteban.castillo@sansano.usm.cl>
2024-11-08 20:06:09 -0300

    Se agrego mas datos a carreras

--- src/components/CarreraCard.jsx
+++ src/components/CarreraCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const CarreraCard = ({ titulo, duracion, imagen }) => {
+const CarreraCard = ({ titulo, profesional, duracion, lugar, imagen }) => {
   return (
     <div className="carrera-card" style={{ backgroundImage: `url(${imagen})` }}>
       <div className="carrera-card__content">
         <h3 className="carrera-card__titulo">{titulo}</h3>
+        <p className="carrera-card__tituloprofesional">{profesional}</p>
         <p className="carrera-card__duracion">{duracion}</p>
+        <p className="carrera-card__lugar">{lugar}</p>
       </div>
     </div>
   );
--- src/pages/CarrerasPage.jsx
+++ src/pages/CarrerasPage.jsx
@@ -7,32 +7,50 @@ const imagenes = require.context('../assets/carreras', false, /\.(png|jpe?g|svg)
 
 const carreras = [
     {   titulo: "Ingeniería Civil",
-        duracion: "6 años",
+        profesional: "Título Profesional: Ingeniero Civil o Ingeniera Civil",
+        duracion: "Duración de los estudios: 6 años",
+        lugar: "Lugar: Campus Casa Central Valparaíso",
+        
         imagen: imagenes("./ingenieria_civil.jpg")
     },
 
     {   titulo: "Ingeniería Comercial",
-        duracion: "5 años",
+        profesional: "Título Profesional: Ingeniero Comercial",
+        duracion: "Duración de los estudios: 5 años",
+        lugar: "Lugar: Campus Casa Central Valparaíso",
+
         imagen: imagenes("./ingenieria-comercial.jpg")
     },
   
     {   titulo: "Arquitectura",
-        duracion: "6 años",
+        profesional: "Título Profesional: Arquitecto o Arquitecta",
+        duracion: "Duración de los estudios: 6 años",
+        lugar: "Lugar: Sede Viña del Mar y Sede Concepción",
+
         imagen: imagenes("./arquitectura.jpg")
     },
   
     {   titulo: "Ingeniería Informática",
-        duracion: "5 años",
+        profesional: "Título Profesional: Ingeniero Informático",
+        duracion: "Duración de los estudios: 6 años",
+        lugar: "Lugar: Sede Viña del Mar y Sede Concepción",
+      
         imagen: imagenes("./ingeniería_informatica.jpg")
     },
   
     {   titulo: "Licenciatura en Física",
-        duracion: "4 años",
+        profesional: "Título Profesional: Licenciado en Física",
+        duracion: "Duración de los estudios: 4 años",
+        lugar: "Lugar: Campus Casa Central Valparaíso",
+
         imagen: imagenes("./fisica.jpg")
     },
   
     {   titulo: "Ingeniería Mecánica",
-        duracion: "5 años",
+        profesional: "Título Profesional: Ingeniero Mecánico",
+        duracion: "Duración de los estudios: 4 años",
+        lugar: "Lugar: Sede Concepción",
+
         imagen: imagenes("./mecanica.jpg")
     },
 ];
